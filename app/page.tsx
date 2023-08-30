323ae82bbf226528802f56fcd7f93d89398f397d
gid-ctrl <gideonusenbor@yahoo.co.uk>
2023-08-30 19:55:02 +0100

    get request implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
 "use client"
 import AddPost from "./components/addPosts"
+import axios from "axios"
+import {useQuery} from "@tanstack/react-query"
+
+const allPosts = async () => {
+  const response = await axios.get("/api/posts/getPosts")
+  return response.data
+}
 
 export default function Home() {
-  return (
+  const { data, error, isLoading } = useQuery({
+    queryFn: allPosts,
+    queryKey: ["posts"],
+  })
+  if (error) return error
+  if (isLoading) return "Loading....."
+  console.log(data)
+return(
     <main>
       <AddPost />
       </main>
--- pages/api/posts/getPosts/index.ts
+++ pages/api/posts/getPosts/index.ts
@@ -10,7 +10,14 @@ export default async function handler(
     if(req.method === "GET"){
     // Get Post
     try{
-        const data = await prisma.post.findMany()
+        const data = await prisma.post.findMany({
+            include: {
+                user: true,
+            },
+            orderBy: {
+                createdAt: "desc"
+            }
+        })
         res.status(200).json(data)
     }catch(err){
         res.status(403).json({err: "Error occured"})
