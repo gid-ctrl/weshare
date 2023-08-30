bb71f3cf5f9f7597002226c03c5318cb54db6885
gid-ctrl <gideonusenbor@yahoo.co.uk>
2023-08-30 17:57:34 +0100

    api implemented

--- pages/api/posts/addPosts/index.ts
+++ pages/api/posts/addPosts/index.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../../auth/[...nextauth]";
+import prisma from "../../../../prisma/client"
 
 export default async function handler(
     req: NextApiRequest,
@@ -11,6 +12,31 @@ export default async function handler(
         const session = await getServerSession(req,res, authOptions)
         if (!session)
         return res.status(401).json({mesage: "Please sign in"})
-    console.log("fetch working",req.body)
+
+        const title: string = req.body.title
+
+        // Get user
+        const prismaUser = await prisma.user.findUnique({
+            where: {email: session?.user?.email},
+        })
+
+        // Check the title is working
+        if(title.length > 300) 
+        return res.status(403).json({message: "Please shorten your share of words"})
+    if(title.length)
+    return res.status(403).json({message: "Please dont leave empty"})
+
+    // Create Post
+    try{
+        const result = await prisma.post.create({
+            data: {
+                title,
+                userId: prismaUser.id,
+            }
+        })
+        res.status(200).json(result)
+    }catch(err){
+        res.status(403).json({err: "Error occured"})
+    }
 }
 }
