bfa1995ba23209735b4abca578f5f9ff1cbbdf58
gid-ctrl <gideonusenbor@yahoo.co.uk>
2023-08-31 11:57:11 +0100

    implemented client side sign in

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,27 @@
-import { getServerSession } from "next-auth"
-import { authOptions } from "../../pages/api/auth/[...nextauth]"
-import { redirect } from "next/navigation"
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../pages/api/auth/[...nextauth]";
+import { redirect } from "next/navigation";
 
-export default async function Dashboard(){
-    const session = await getServerSession(authOptions)
-    if(!session){
-        redirect('/api/auth/signin')
+export default async function Dashboard() {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+        redirect('/api/auth/signin');
     }
-    return(
-        <main>
-            <h1 className="text-2xl font-bold text-white">Welcome Back {session?.user?.name}</h1>
+    
+    return (
+        <main className="bg-gray-800 min-h-screen flex flex-col justify-center items-center">
+            <div className="max-w-md p-8 bg-white rounded-lg shadow-md text-center">
+                
+                <h1 className="text-2xl font-bold text-gray-800">
+                    Welcome Back, {session?.user?.name}
+                </h1>
+                <p className="text-gray-600 mt-2">
+                    Join the conversation and share your feelings with the group!
+                </p>
+                <button className="mt-4 px-4 py-2 bg-gray-800 text-white rounded-md">
+                    Start Sharing
+                </button>
+            </div>
         </main>
-    )
+    );
 }
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "../../../prisma/client";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
@@ -9,6 +10,32 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username:",
+          type: "text",
+          placeholder: "your username",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      async authorize(credentials) {
+        const user = { id: 1, name: "Gideon", password: "password" };
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
     }),
   ],
 };
